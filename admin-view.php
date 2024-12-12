da779eae15f642f769ae09ddee397fa17d194b95
Chris Blair <cblair92@gmail.com>
2024-12-12 11:25:38 -0500

    nobr looks better - removed style

--- admin-view.php
+++ admin-view.php
@@ -254,18 +254,21 @@ if (!empty($_POST)) {
                                                     echo $event_text;
                                                 } ?>
                                             </p>
-                                            <p style="margin: 0; font-family: Arial,sans-serif; text-align: right; white-space: nowrap; hyphens: none;">
+                                            <p style="margin: 0; font-family: Arial,sans-serif; text-align: right;">
                                                 <nobr>
                                                 <?php
                                                 $event_buttons = ia_email_get_buttons($event->id);
+                                                $left_margin = "0px";
                                                 foreach ($event_buttons as $event_button) { ?>
                                                     <a href="<?php echo stripslashes($event_button->event_button_link); ?>" 
-                                                        style="background: #ffffff; border: 2px solid #8dc1d6; text-decoration: none; padding: 10px 8px; margin: 2px;
+                                                        style="background: #ffffff; border: 2px solid #8dc1d6; text-decoration: none; padding: 10px 8px; 
+                                                            margin: 0px 0px 0px <?php echo $left_margin; ?>;
                                                             color: #000000; border-radius: 4px; display: inline-block; mso-padding-alt: 0; text-underline-color: #ffffff;">
                                                         <span style="mso-text-raise: 10pt; font-weight: bold;">
                                                             <?php echo stripslashes($event_button->event_button_text); ?>
                                                         </span></a>
                                                 <?php
+                                                $left_margin = "4px";
                                                 } ?>
                                                 </nobr>
                                             </p>
